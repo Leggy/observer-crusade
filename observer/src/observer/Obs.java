ed46a2e4dc4cbffbdadfe9c97fccbea3893c9e3d
Leggy <lachlan.j.healey@gmail.com>
2013-08-31 17:22:04 +1000

    Finished Observer Method

--- observer/src/observer/Obs.java
+++ observer/src/observer/Obs.java
@@ -1,7 +1,9 @@
 package observer;
 
+import java.util.HashSet;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Set;
 
 import model.Model;
 
@@ -9,8 +11,30 @@ import model.Model;
 public class Obs implements Observer {
 
 	@Override
-	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
+	public void update(Observable observable, Object object) {
+		System.out.println("OBS: Update called");
+		
+		/*
+		 * Checking if observable is instance of model
+		 */
+		if(observable instanceof Model){
+			System.out.println("OBS: Observable is an instanceof Model");
+			
+			/*
+			 * Checking if object is instance of string and set
+			 */
+			if(object instanceof String){
+				System.out.println("OBS: Object is an instanceof String");
+			} else if(object instanceof Set){
+				System.out.println("OBS: Object is an instanceof Set");
+			} else if(object instanceof HashSet){
+				System.out.println("OBS: Object is an instanceof HashSet");
+			}
+		} else {
+			System.out.println("OBS: I HAVE NO IDEA WHAT I AM DOING!");
+		}
+		
+		
 		
 	}
 
