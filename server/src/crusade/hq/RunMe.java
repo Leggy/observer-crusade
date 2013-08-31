0ea6ccc63e47e2e60f77bbad6eeba9f188dd72df
Leggy <lachlan.j.healey@gmail.com>
2013-08-31 17:42:08 +1000

    Finished RunMe

--- server/src/crusade/hq/RunMe.java
+++ server/src/crusade/hq/RunMe.java
@@ -1,11 +1,58 @@
 package crusade.hq;
 
+import observer.Obs;
+import model.Model;
+
 public class RunMe {
-	
-	public RunMe() {
+
+	private static Model model;
+
+	private static Obs obs;
+
+	public static void main(String[] args) {
+
+		model = new Model();
+
+		obs = new Obs();
+
+		model.addObserver(obs);
+
+		System.out.println("RUN: initialised");
+
+		/*
+		 * Changing id
+		 */
+		System.out.println("RUN: Changing ID");
+		model.setID(model.getID() + 4);
 		
+		
+		/*
+		 * Adding new player String
+		 */
+		System.out.println("RUN: Adding new player String");
+		model.addPlayer("Andrew");
+		
+		
+		/*
+		 * Changing id
+		 */
+		System.out.println("RUN: Changing ID");
+		model.setID(model.getID() + 6);
+		
+		
+		/*
+		 * Adding new player String
+		 */
+		System.out.println("RUN: Adding new player String");
+		model.addPlayer("Jordan");
+		
+		
+		
+		
+		
+
 	}
-	
+
 	public void print() {
 		System.out.println("SVR: hhhehehe");
 	}
